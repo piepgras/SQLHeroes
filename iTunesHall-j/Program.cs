d02eb8aca4dfde40b7f5d1730b0d6aa423628c6c
Michael Piepgras Neergaard <michael@neergaard.dk>
2023-02-20 11:54:53 +0100

    Initial Commit

---
+++ iTunesHall-j/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace iTunesHall_j
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
